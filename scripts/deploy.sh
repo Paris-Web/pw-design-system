8bdca105f9dcf7c0611b3d016c138af93fd5761d
Pierre Martin <pierre@occitech.fr>
2017-12-19 13:45:26 +0100

    Version initiale de la config CircleCI avec déploiement

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$CIRCLE_CI" ]; then
+  BRANCH_SLUG=`$CIRCLE_BRANCH | tr "/" "-"`
+else
+  BRANCH_SLUG=`git rev-parse --abbrev-ref HEAD | tr "/" "-"`
+fi
+
+echo "Deploying to remote folder '$BRANCH_SLUG'"
+
+rsync -avzL --delete --chmod=755 dist/ sites@www.paris-web.fr:/srv/sites/stage/www/test/design-system/$BRANCH_SLUG
