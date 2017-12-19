4e09142013093368c02f2defec00e290ede3acc9
Julien <julien.pradet@gmail.com>
2017-12-19 21:33:50 +0100

    Improve uglify/production env of built js files

--- webpack.conf.js
+++ webpack.conf.js
@@ -8,21 +8,35 @@ export default {
         test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
         loader: "file-loader?name=/[hash].[ext]"
       },
-      {test: /\.json$/, loader: "json-loader"},
+      { test: /\.json$/, loader: "json-loader" },
       {
         loader: "babel-loader",
         test: /\.js?$/,
         exclude: /node_modules/,
-        query: {cacheDirectory: true}
+        query: { cacheDirectory: true }
       }
     ]
   },
 
   plugins: [
     new webpack.ProvidePlugin({
-      "fetch": "imports-loader?this=>global!exports?global.fetch!whatwg-fetch"
+      fetch: "imports-loader?this=>global!exports?global.fetch!whatwg-fetch"
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || "production")
+      }
     })
-  ],
+  ].concat(
+    process.env.NODE_ENV === "production"
+      ? [
+          new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: { warnings: false }
+          })
+        ]
+      : []
+  ),
 
   context: path.join(__dirname, "src"),
   entry: {
@@ -33,5 +47,5 @@ export default {
     publicPath: process.env.HUGO_BASEURL || "/",
     filename: "[name].js"
   },
-  externals:  [/^vendor\/.+\.js$/]
+  externals: [/^vendor\/.+\.js$/]
 };
