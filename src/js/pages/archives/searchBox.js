313a5750f0f9d9262edd6c209fdca7fed9617d79
Joachim Robert <joachim.robert@protonmail.com>
2020-05-01 15:28:53 +0200

    Add clear serchbar

--- src/js/pages/archives/searchBox.js
+++ src/js/pages/archives/searchBox.js
@@ -22,16 +22,26 @@ const searchBox = connectSearchBox((options, isFirstRender) => {
 
     const container = document.querySelector(options.widgetParams.container);
     const input = container.querySelector("input");
+    const clearButton = container.querySelector("#clearSearch");
     if (input.value !== options.query) {
       input.value = options.query;
+      clearButton.removeAttribute("hidden");
     }
 
+    clearButton.addEventListener("click", function(event) {
+      refine.cancel();
+      options.clear();
+      input.value = "";
+      clearButton.setAttribute("hidden", "hidden");
+    })
     input.addEventListener("keyup", function(event) {
       if (event.target.value === "") {
         refine.cancel();
         options.clear();
+        clearButton.setAttribute("hidden", "hidden");
       } else {
         refine(event.target.value);
+        clearButton.removeAttribute("hidden");
       }
     });
   }
