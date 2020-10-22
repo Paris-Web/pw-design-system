5d4cc4a39e7ba3edb700772d33c7a1bebada18f1
Joachim <joachim.robert@protonmail.com>
2020-10-22 12:39:42 +0200

    [Hub] Small updates (#113)

--- src/js/pages/hub/enabled-at.js
+++ src/js/pages/hub/enabled-at.js
@@ -2,10 +2,17 @@
 //   data-enabled-at-start="2020-10-01 10:00:00"
 //   data-enabled-at-end="2020-10-01 11:00:00"
 
+// Using `Date.parse` does not work consistently on all browsers. 
+const dateParse = (string) => {
+  var s = string.split(/[^0-9]/);
+  return new Date(s[0], s[1] - 1, s[2], s[3], s[4], s[5]);
+}
+
 export const checkEnabledAt = () => {
   document.querySelectorAll("[data-enabled-at-start]").forEach((elem) => {
-    const start = Date.parse(elem.dataset.enabledAtStart);
-    const end = Date.parse(elem.dataset.enabledAtEnd);
+    const start = dateParse(elem.dataset.enabledAtStart);
+    const end = dateParse(elem.dataset.enabledAtEnd);
+
     const now = Date.now();
 
     if (start < now && now < end) {
